73f8a1f236a1a43a443e6357701d91a16562ca27
Jamie Meyer <45072324+HeatXD@users.noreply.github.com>
2024-03-11 17:06:59 +0100

    Keys implemented

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,31 @@ mod chip8;
 use std::env;
 
 use chip8::CHIP8;
-use macroquad::prelude::*;
+use macroquad::{
+    audio::{
+        load_sound, play_sound, stop_sound, PlaySoundParams,
+    },
+    prelude::*,
+};
+
+const KEYS: [KeyCode; 16] = [
+    KeyCode::X,
+    KeyCode::Key1,
+    KeyCode::Key2,
+    KeyCode::Key3,
+    KeyCode::Q,
+    KeyCode::W,
+    KeyCode::E,
+    KeyCode::A,
+    KeyCode::S,
+    KeyCode::D,
+    KeyCode::Z,
+    KeyCode::C,
+    KeyCode::Key4,
+    KeyCode::R,
+    KeyCode::F,
+    KeyCode::V,
+];
 
 #[macroquad::main("CHIP-8 Emulator By HeatXD")]
 async fn main() {
@@ -12,23 +36,47 @@ async fn main() {
         panic!("NO ROM GIVEN");
     }
 
-    let mut image = Image::gen_image_color(64, 32, BLACK);
+    // load beep sound
+    set_pc_assets_folder("assets");
+    let beep = load_sound("beep.wav").await.expect("Couldn't load audio");
 
+    // set window size following 64x32 ratio
+    request_new_screen_size(1024., 512.);
+
+    // drawing prep
+    let mut image = Image::gen_image_color(64, 32, BLACK);
     let texture = Texture2D::from_image(&image);
     texture.set_filter(FilterMode::Nearest);
 
-    request_new_screen_size(1024., 512.);
-
+    // setup chip8 and load rom
     let mut cpu = CHIP8::default();
-
     cpu.load_rom(&args[1]);
 
     while !is_quit_requested() {
         cpu.cycle();
 
-        // todo poll input
+        for (idx, key) in KEYS.iter().enumerate() {
+            cpu.set_key(idx, is_key_down(*key));
+        }
+
+        if cpu.did_beep() {
+            // stop if its still playing
+            stop_sound(&beep);
+            play_sound(
+                &beep,
+                PlaySoundParams {
+                    volume: 0.2,
+                    looped: false,
+                },
+            );
+        }
+
         clear_background(WHITE);
-        update_image(&cpu, &mut image, &texture);
+
+        if cpu.did_draw() {
+            update_image(&cpu, &mut image, &texture);
+        }
+        
         draw_texture_ex(
             &texture,
             0.,
@@ -39,6 +87,7 @@ async fn main() {
                 ..Default::default()
             },
         );
+
         next_frame().await
     }
 }
